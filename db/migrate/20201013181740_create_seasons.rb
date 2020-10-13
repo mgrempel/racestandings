99b6febb8699f6c524159f32fcc5b778d8ced09f
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-10-13 13:17:49 -0500

    added season model

---
+++ db/migrate/20201013181740_create_seasons.rb
@@ -0,0 +1,10 @@
+class CreateSeasons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :seasons do |t|
+      t.integer :year
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
