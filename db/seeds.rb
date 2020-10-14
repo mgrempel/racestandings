c8ade8b5d91baadcc7c3d7e2866166fd1c4739dc
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-10-14 16:59:37 -0500

    Added seeding for drivers and seasons

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,32 @@ Driver.destroy_all
 
 # API endpoints
 drivers_url = "http://ergast.com/api/f1/drivers.json"
-# seasons_URL = "http://ergast.com/api/f1/seasons.json"
+seasons_url = "http://ergast.com/api/f1/seasons.json"
 
+# Populate drivers
 driver_uri = URI(drivers_url)
 response = Net::HTTP.get(driver_uri)
 driver_data = JSON.parse(response)
 
 drivers = driver_data["MRData"]["DriverTable"]["Drivers"]
 drivers.each do |driver|
-  Driver.create(first_name: driver["givenName"],
-                last_name:  driver["familyName"])
+  Driver.create(first_name:  driver["givenName"],
+                last_name:   driver["familyName"],
+                dob:         driver["dateOfBirth"],
+                url:         driver["url"],
+                nationality: driver["nationality"],
+                driverid:    driver["driverId"])
+end
+
+# Populate seasons
+season_uri = URI(seasons_url)
+season_response = Net::HTTP.get(season_uri)
+season_data = JSON.parse(season_response)
+
+seasons = season_data["MRData"]["SeasonTable"]["Seasons"]
+seasons.each do |season|
+  Season.create(year: season["season"],
+                url:  season["url"])
 end
 
 # handle driver data
