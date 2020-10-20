8ff665b683414de004802785d79cc9f5cb9ed53c
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-10-20 15:36:23 -0500

    Added drivers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'drivers/index'
   get "home/index"
   get "home/about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
