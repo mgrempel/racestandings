68c3237fd6b7259f06ce593c915c05d922ab2219
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-10-20 20:05:15 -0500

    3.2 Added individual page for Drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,4 +2,8 @@ class DriversController < ApplicationController
   def index
     @drivers = Driver.all
   end
+
+  def show
+    @driver = Driver.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/circuits/:id", to: "circuits#show", as: "circuit"
   get "laptimes/index"
   get "drivers/index"
+  get "/drivers/:id", to: "drivers#show", as: "driver"
   get "home/index"
   get "home/about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
