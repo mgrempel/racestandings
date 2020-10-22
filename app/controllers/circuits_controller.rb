898f5a59cfc6e1cd252a4c91eb7312f23017db88
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-10-22 18:35:36 -0500

    4.1 added search capabilities to Circuits

--- app/controllers/circuits_controller.rb
+++ app/controllers/circuits_controller.rb
@@ -1,6 +1,15 @@
 class CircuitsController < ApplicationController
   def index
-    @circuits = Circuit.all
+    # @circuits = Circuit.all
+    # @circuits = Circuit.where(name: params[:search]) unless params[:search] == ""
+
+    @circuits = if params[:search] == ""
+                  Circuit.all
+                else
+                  Circuit.where(name: params[:search])
+                end
+
+    @categories = ["Circuits", "Seasons", "Laptimes"]
   end
 
   def show
