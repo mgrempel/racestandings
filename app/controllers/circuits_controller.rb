11c64c4cc216d34034a566d5ee62b7de09075207
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-10-22 19:27:17 -0500

    4.2 added category selection to search for circuit

--- app/controllers/circuits_controller.rb
+++ app/controllers/circuits_controller.rb
@@ -1,14 +1,16 @@
 class CircuitsController < ApplicationController
   def index
-    # @circuits = Circuit.all
-    # @circuits = Circuit.where(name: params[:search]) unless params[:search] == ""
-
-    @circuits = if params[:search] == ""
-                  Circuit.all
-                else
-                  Circuit.where(name: params[:search])
-                end
-
+    root_query = Circuit.includes(:seasons, :laptimes).all
+    unless params[:search] == ""
+      root_query = if params[:catagory] == "Circuits"
+                     root_query.where(name: params[:search])
+                   elsif params[:category] == "Seasons"
+                     root_query.where(seasons: { year: params[:search] })
+                   else
+                     root_query.where(laptimes: { laptime: params[:search] })
+                   end
+    end
+    @circuits = root_query
     @categories = ["Circuits", "Seasons", "Laptimes"]
   end
 
