bd2c2d3a52ff9f4b3cebc49e8311e8a2cf761beb
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-10-25 14:41:37 -0500

    4.1/4.2 drivers - implmented search functionality

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,6 +1,16 @@
 class DriversController < ApplicationController
   def index
-    @drivers = Driver.all
+    root_query = Driver.includes(:laptimes).all
+    unless params[:search] == ""
+      root_query = if params[:category] == "Drivers"
+                     root_query.where(first_name: params[:search]).or(root_query.where(last_name: params[:search]))
+                   else
+                     root_query.where(laptimes: { laptime: params[:search] })
+                   end
+
+    end
+    @drivers = root_query
+    @categories = ["Drivers", "Laptimes"]
   end
 
   def show
