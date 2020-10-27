8dc330ed9a02d5e5c283573e2d71a769a3558724
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-10-26 21:03:34 -0500

    4.1/4.1 implemented search for laptimes

--- app/controllers/laptimes_controller.rb
+++ app/controllers/laptimes_controller.rb
@@ -1,6 +1,20 @@
 class LaptimesController < ApplicationController
   def index
-    @laptimes = Laptime.all
+    root_query = Laptime.includes(:season, :driver, :circuit).all
+
+    unless params[:search] == ""
+      root_query = if params[:catagory] == "Laptimes"
+                     root_query.where(laptime: params[:search])
+                   elsif params[:category] == "Circuits"
+                     root_query.where(circuits: { name: params[:search] })
+                   elsif params[:category] == "Seasons"
+                     root_query.where(Seasons: { year: params[:search] })
+                   else
+                     root_query.where(Drivers: { first_name: params[:search] }).or(root_query.where(Drivers: { last_name: params[:search] }))
+                   end
+    end
+    @laptimes = root_query
+    @categories = ["Laptimes", "Circuits", "Seasons", "Drivers"]
   end
 
   def show
